9a50a8f9b3bb83bc342f5edc90c08eac0cecfd19
= <=>
2020-12-16 18:15:04 +0300

    add timer

--- script.js
+++ script.js
@@ -199,3 +199,41 @@ squareNotRepeat = (sudokuGrid) => {
         }
     } return true;
 }
+
+
+window.onload = ()=> {
+    let hr = $('#hours');
+    let min = $('#minutes');
+    let sec = $('#seconds');
+    let counter = 0;
+    $('#start').on('click', start);
+    $('#pause').on('click', function () {
+        clearInterval(counter);
+        $('#start').on('click', start);
+    });
+    function start() {
+        counter = setInterval(step, 1000);
+        $('#start').off('click');
+    }
+
+    function step() {
+        sec.val(+sec.val() + 1);
+        if (sec.val() > 59) {
+            sec.val('00');
+            min.val(+min.val() + 1);
+            min.text(formatTime(min.val()));
+        }
+        if (min.val() > 59) {
+            min.val('00');
+            hr.val(+hr.val() + 1);
+            min.text(formatTime(min.val()));
+            hr.text(formatTime(hr.val()));
+        }
+        sec.text(formatTime(sec.val()));
+    }
+ 
+    function formatTime(value) {
+        let partial = `${('0' + value % 60).slice(-2)}`;
+        return partial;
+    }
+}
